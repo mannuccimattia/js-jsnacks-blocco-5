7747077e5d5ace0fab95287973b47c58bf00555d
mannuccimattia <mannuccimattia@gmail.com>
2025-04-02 14:47:32 +0200

    [UPD] Done snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,5 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+const formattedNames = names.map(name => `'${name}'`);
+console.log(formattedNames.join(", "));
