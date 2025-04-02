2a01e39d40d31a476a80206aeb0e9aac52d32320
mannuccimattia <mannuccimattia@gmail.com>
2025-04-02 15:09:13 +0200

    [UPD] Done snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const student = students.find(stud => stud.name === "Marco Lanci");
+const studentClass = student.class;
+console.log(studentClass);
