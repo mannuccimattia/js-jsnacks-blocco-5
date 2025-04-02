ad30f7bcb2e49161c46ec705455113ec4f5d4396
mannuccimattia <mannuccimattia@gmail.com>
2025-04-02 15:04:07 +0200

    [UPD] Done snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,5 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+const myStudent = students.find(stud => stud.id === 2);
+console.log(myStudent);
