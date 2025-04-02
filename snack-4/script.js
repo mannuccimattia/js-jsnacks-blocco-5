591b9c58c655a8456c60bd3f5b76f31590522c62
mannuccimattia <mannuccimattia@gmail.com>
2025-04-02 14:57:24 +0200

    [UPD] Done snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,5 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const authors = posts.map(name => `'${name.author}'`);
+console.log(authors.join(", "));
